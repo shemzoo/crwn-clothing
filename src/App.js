d0f3f05ea87aee37bc55ad9943f28a6b7486b684
shemzoo <lvn-gg@yandex.ru>
2025-03-06 12:58:40 +0300

    add react router, navigation component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
-import CategoriesMenu from "./components/categories-menu/categories-menu.component";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./components/routes/home/home.component";
+import Navigation from "./components/routes/navigation/navigation.component";
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "Womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "Mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
   return (
-    <div className="categories-container">
-      <CategoriesMenu categories={categories} />
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={<Navigation />}
+      >
+        <Route
+          index
+          element={<Home />}
+        />
+        <Route
+          path="shop"
+          element={<Shop />}
+        />
+      </Route>
+    </Routes>
   );
 };
 
